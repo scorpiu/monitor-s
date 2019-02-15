a00b7a2168e98f3f3a77f5940a047a462d5ecbda
18616964416@189.cn <18616964416@189.cn>
2019-02-15 10:55:31 +0800

    春哥春哥春哥~~~~！！

--- src/main/java/com/ideal/Application.java
+++ src/main/java/com/ideal/Application.java
@@ -15,8 +15,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        //hahahahahahahaha
-        //hahahahahahahaha
     }
 
 }
